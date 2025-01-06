2b372ede951705e622e8f991d7712a506d18358b
Samuel Michael Vanie <samuelmichaelvanie@Samuels-MacBook-Air.local>
2025-01-06 06:52:07 +0100

    ci: setting up the whole new project
    - add leptos with SSR
    - add flake environment

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "My resume generator application flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        vdebuggerBuild = rustPlatform.buildRustPackage {
+          pname = "resumegenerator";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+      in
+      {
+        defaultPackage = vdebuggerBuild;
+
+        devShell = pkgs.mkShell {
+          buildInputs = [ (rustVersion.override { extensions = [ "rust-src" ]; }) pkgs.dwarfdump ];
+        };
+
+      });
+}
